666682edfa4bc52023436b77ae06e0e8bfcc72d3
Isabela Macedo <isabelacb.macedo@gmail.com>
2022-07-27 20:43:11 -0300

    feat: Renderizando um array

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
+import { List } from "rbx";
 import React from "react";
 import Botao from "./components/Button";
 import Formulario from "./components/Button/Form";
+import Lista from "./components/Lista";
 
 function App() {
 	return (
 		<div className="App">
 			<Formulario />
+			<Lista />
 		</div>
 	);
 }
