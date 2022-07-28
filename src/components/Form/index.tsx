d57887fd8a307274f587a95e3acb761fb3913985
Isabela Macedo <isabelacb.macedo@gmail.com>
2022-07-27 22:43:20 -0300

    feat: controlando o formulário

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -1,30 +1,51 @@
-import React from "react";
-import Botao from "../Button";
-import style from "./Form.module.scss";
+import React from 'react';
+import Botao from '../Button';
+import style from './Form.module.scss';
 
 class Formulario extends React.Component {
+	state = {
+		tarefa: '',
+		tempo: '00:00',
+	};
+
+	adicionarTarefa(evento: React.FormEvent<HTMLFormElement>) {
+		evento.preventDefault();
+		console.log('state: ', this.state);
+	}
+
 	render() {
 		return (
-			<form className={style.novaTarefa}>
+			<form
+				className={style.novaTarefa}
+				onSubmit={this.adicionarTarefa.bind(this)}
+			>
 				<div className={style.inputContainer}>
-					<label htmlFor="tarefa"> Adicione um novo estudo</label>
+					<label htmlFor='tarefa'> Adicione um novo estudo</label>
 					<input
-						type="text"
-						name="tarefa"
-						id="tarefa"
-						placeholder="O que você quer estudar?"
+						type='text'
+						name='tarefa'
+						id='tarefa'
+						value={this.state.tarefa}
+						onChange={(evento) =>
+							this.setState({ ...this.state, tarefa: evento.target.value })
+						}
+						placeholder='O que você quer estudar?'
 						required
 					/>
 				</div>
 				<div className={style.inputContainer}>
-					<label htmlFor="tempo">Tempo</label>
+					<label htmlFor='tempo'>Tempo</label>
 					<input
-						type="time"
-						name="tempo"
-						id="tempo"
-						step="1"
-						min="00:00:00"
-						max="01:30:00"
+						type='time'
+						name='tempo'
+						value={this.state.tempo}
+						onChange={(evento) =>
+							this.setState({ ...this.state, tempo: evento.target.value })
+						}
+						id='tempo'
+						step='1'
+						min='00:00:00'
+						max='01:30:00'
 						required
 					/>
 				</div>
