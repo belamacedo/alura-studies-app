9018dbebb9c3037d089b8d20f263eb00030a0d29
Isabela Macedo <isabelacb.macedo@gmail.com>
2022-07-27 21:42:17 -0300

    feat: button props

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,9 +1,12 @@
 import React from "react";
 import style from "./Botao.module.scss";
 
-class Botao extends React.Component {
+interface IProps {
+	children: React.ReactNode;
+}
+class Botao extends React.Component<IProps> {
 	render() {
-		return <button className={style.botao}>Botão</button>;
+		return <button className={style.botao}>{this.props.children}</button>;
 	}
 }
 
