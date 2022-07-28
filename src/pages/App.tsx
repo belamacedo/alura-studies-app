489a06f7f81bb32ce5acf2db6194138821c1533a
Isabela Macedo <isabelacb.macedo@gmail.com>
2022-07-27 21:12:58 -0300

    feat: estilizando com Sass

---
+++ src/pages/App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Formulario from "../components/Form";
+import Lista from "../components/Lista";
+import "./style.scss";
+
+function App() {
+	return (
+		<div className="AppStyle">
+			<Formulario />
+			<Lista />
+		</div>
+	);
+}
+
+export default App;
