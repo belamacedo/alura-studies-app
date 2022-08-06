e3aa7c6b5fc7b6824b324ec2153436d4831f2c58
isabelamacedo <isabelacb.macedo@gmail.com>
2022-08-05 23:07:05 -0300

    feat: mostrando tempo no relógio

--- src/components/Cronometro/Relogio/index.tsx
+++ src/components/Cronometro/Relogio/index.tsx
@@ -1,14 +1,24 @@
-import React from "react";
-import style from "./Relogio.module.scss";
+import React from 'react';
+import style from './Relogio.module.scss';
 
-export default function Relogio() {
-	return (
-		<>
-			<span className={style.relogioNumero}>0</span>
-			<span className={style.relogioNumero}>0</span>
-			<span className={style.relogioNDivisao}>:</span>
-			<span className={style.relogioNumero}>0</span>
-			<span className={style.relogioNumero}>0</span>
-		</>
-	);
+interface Props {
+  tempo: number | undefined;
+}
+
+export default function Relogio({ tempo = 0 }: Props) {
+  // se o tempo não exister/for undefined, será setado o tempo 0.
+
+  const minutos = Math.floor(tempo / 60);
+  const segundos = tempo % 60;
+  const [minutoDezena, minutoUnidade] = String(minutos).padStart(2, '0'); //1º parametro é a quantidade de caracter e 2º parametro é o valor default dele
+  const [segundoDezena, segundoUnidade] = String(segundos).padStart(2, '0');
+  return (
+    <>
+      <span className={style.relogioNumero}>{minutoDezena}</span>
+      <span className={style.relogioNumero}>{minutoUnidade}</span>
+      <span className={style.relogioNDivisao}>:</span>
+      <span className={style.relogioNumero}>{segundoDezena}</span>
+      <span className={style.relogioNumero}>{segundoUnidade}</span>
+    </>
+  );
 }
